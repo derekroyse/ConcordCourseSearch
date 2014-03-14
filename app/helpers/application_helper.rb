301b02f5859750ebfe7b79bf07ec11a78cf5aad0
derekroyse <derekroyse@gmail.com>
2014-03-13 22:22:19 -0400

    basic searching implemented

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,8 @@
 module ApplicationHelper
   @@choice = 201402
-  
+  @@headers = Array.new(18)
+  @@records = Array.new(18){Array.new}
+  @@numRecords = 0
   # Returns the full title on a per-page basis.
   def full_title(page_title)
     base_title = "Ruby on Rails Tutorial Sample App"
@@ -18,8 +20,7 @@ module ApplicationHelper
     j = 0
     x = 0
     y = 0
-    headers = Array.new(18)
-    records = Array.new(18){Array.new}
+
     agent = Mechanize.new
     data = agent.get('https://apps.concord.edu/schedules/seatstaken.php')
     select_list = data.form_with(:action => '/schedules/seatstaken.php')
@@ -32,18 +33,18 @@ module ApplicationHelper
     
     data = agent.submit(select_list)
     rows = data.search("td")
-    numRecords = (rows.length/18)-(rows.length/900)-1
+    @@numRecords = (rows.length/18)-(rows.length/900)-1
     
     # Populate headers and row data.
     while i < rows.length
       if i < 18
-	headers[i] = rows[i].text
+	@@headers[i] = rows[i].text
 	i+=1
       elsif i % 918 <= 17
 	i+=1
       else
 	while y < 18
-	  (records[x] ||= [])[y] = rows[i].text
+	  (@@records[x] ||= [])[y] = rows[i].text
 	  y+=1
 	  i+=1
 	end #end while
@@ -53,13 +54,13 @@ module ApplicationHelper
     end # end while
     
     formatted_headers = "<table><tr class='table_header'>"
-    for element in headers
+    for element in @@headers
       formatted_headers += "<td>" + element + "</td>"
     end
     formatted_headers += "</tr>"
    
     formatted_records = ""
-    records.each_with_index do |row, xi|
+    @@records.each_with_index do |row, xi|
       formatted_records += "<tr>"
       row.each_with_index do |column, yi|
 	formatted_records += "<td>"+ column + "</td>"
@@ -73,4 +74,38 @@ module ApplicationHelper
     #return formatted_final
   end # end scrape_site
   
-end #end module
+  def search(crn, subj, crs)
+
+     x = 0
+     formatted_headers = "<table><tr class='table_header'>"
+         for element in @@headers
+	  formatted_headers += "<td>" + element + "</td>"
+	 end #end for
+    formatted_headers += "</tr>"
+    formatted_records = ""
+    
+     while x < @@numRecords
+      if @@records[x][0].index(crn) != nil ||
+	 @@records[x][1].index(subj) != nil ||
+	 @@records[x][2].index(crs) != nil 
+	#&& @@records[x][3].index(SEC) != nil &&
+#	@@records[x][4].index(TITLE) != nil && @@records[x][5].index(CH) != nil &&
+#	@@records[x][6].index(MAX) != nil && @@records[x][7].index(ENR) != nil && 
+#	@@records[x][8].index(AVAIL) != nil && @@records[x][9].index(WL) != nil &&
+#	@@records[x][10].index(DAYS) != nil && @@records[x][11].index(STIME) != nil &&
+#	@@records[x][12].index(ETIME) != nil &&	@@records[x][13].index(BLDGROOM) != nil &&
+#	@@records[x][14].index(WK) != nil && @@records[x][15].index(INSTRUCTOR) != nil &&
+#	@@records[x][16].index(EF) != nil && @@records[x][17].index(STARTS) != nil &&
+	formatted_records += "<tr>"
+	  for column in @@records[x]
+	    formatted_records += "<td>"+ column + "</td>"
+	  end #end for
+	formatted_records += "</tr>"
+      end # end if
+      x+=1
+  end # end while
+  formatted_records += "</table>"
+  formatted_final = formatted_headers + formatted_records
+end #end method
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ SampleApp::Application.routes.draw do
   root 'static_pages#home'
   match '/startup', to: 'scraper#startup', via: 'get'
   match '/startup', to: 'scraper#startup', via: 'post'
+  match '/search', to: 'scraper#search', via: 'get'
+  match '/search', to: 'scraper#search', via: 'post'
   match '/signup', to: 'users#new', via:'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
