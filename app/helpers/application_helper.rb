9138d1ab10199d8cdec507c74666b04844af3229
derekroyse <derekroyse@gmail.com>
2014-03-15 22:21:46 -0400

    inclusive search fully implemented

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  @@choice = 201402
+  @@choice = 201501
   @@headers = Array.new(18)
   @@records = Array.new(18){Array.new}
   @@numRecords = 0
@@ -22,11 +22,11 @@ module ApplicationHelper
     y = 0
 
     agent = Mechanize.new
-    data = agent.get('https://apps.concord.edu/schedules/seatstaken.php')
+    data = agent.get('https://apps.concord.edu/schedules/seatstaken.php') 
     select_list = data.form_with(:action => '/schedules/seatstaken.php')
  
     if @@choice.inspect == "nil"
-      select_list.field_with(:name =>"term").value = 201402
+      select_list.field_with(:name =>"term").value = 201501
     else
       select_list.field_with(:name =>"term").value = @@choice
     end
@@ -85,15 +85,24 @@ module ApplicationHelper
     formatted_records = ""
     
      while x < @@numRecords
-      if  @@records[x][0].index(crn) != nil || @@records[x][1].index(subj) != nil ||
-	  @@records[x][2].index(crs) != nil ||	 @@records[x][3].index(sec) != nil ||
-	  @@records[x][4].index(title) != nil || @@records[x][5].index(ch) != nil ||
-	  @@records[x][6].index(max) != nil || 	 @@records[x][7].index(enr) != nil || 
-	  @@records[x][8].index(avail) != nil || @@records[x][9].index(wl) != nil ||
-	  @@records[x][10].index(days) != nil || @@records[x][11].index(stime) != nil ||
-	  @@records[x][12].index(etime) != nil ||@@records[x][13].index(bldgroom) != nil ||
-	  @@records[x][14].index(wk) != nil ||   @@records[x][15].index(instructor) != nil ||
-	  @@records[x][16].index(ef) != nil || 	 @@records[x][17].index(starts) != nil
+      if  @@records[x][0] == crn && crn.inspect != "\"\"" && @@records[x][0].inspect != "\"\"" || 
+	  @@records[x][1].downcase.index(subj.downcase) != nil && subj.inspect != "\"\"" && @@records[x][0].inspect != "\"\""  ||
+	  @@records[x][2].downcase.index(crs.downcase) != nil && crs.inspect != "\"\"" && @@records[x][0].inspect != "\"\""  || 
+	  @@records[x][3].downcase.index(sec.downcase) != nil && sec.inspect != "\"\"" && @@records[x][0].inspect != "\"\""  ||
+	  @@records[x][4].downcase.index(title.downcase) != nil  && title.inspect != "\"\"" && @@records[x][0].inspect != "\"\""  || 
+	  @@records[x][5] == ch && ch.inspect != "\"\"" && @@records[x][0].inspect != "\"\""  ||
+	  @@records[x][6] == max && max.inspect != "\"\"" && @@records[x][0].inspect != "\"\""  || 
+	  @@records[x][7] == enr && enr.inspect != "\"\"" && @@records[x][0].inspect != "\"\""  || 
+	  @@records[x][8] == avail && avail.inspect != "\"\"" && @@records[x][0].inspect != "\"\""  ||
+	  @@records[x][9] == wl && wl.inspect != "\"\"" && @@records[x][0].inspect != "\"\""  ||
+	  @@records[x][10].downcase.index(days.downcase) != nil && @@records[x][0].inspect != "\"\""   && days.inspect != "\"\"" || 
+	  @@records[x][11].downcase.index(stime.downcase) != nil && @@records[x][0].inspect != "\"\""  && stime.inspect != "\"\"" ||
+	  @@records[x][12].downcase.index(etime.downcase) != nil && etime.inspect != "\"\"" && @@records[x][0].inspect != "\"\""  || 
+	  @@records[x][13].downcase.index(bldgroom.downcase) != nil && bldgroom.inspect != "\"\"" && @@records[x][0].inspect != "\"\""  ||
+	  @@records[x][14].downcase.index(wk.downcase) != nil 	&& wk.inspect != "\"\"" && @@records[x][0].inspect != "\"\""  || 
+	  @@records[x][15].downcase.index(instructor.downcase) != nil && instructor.inspect != "\"\"" && @@records[x][0].inspect != "\"\""  ||
+	  @@records[x][16].downcase.index(ef.downcase) != nil 	 && ef.inspect != "\"\"" && @@records[x][0].inspect != "\"\""  || 
+	  @@records[x][17].downcase.index(starts.downcase) != nil && starts.inspect != "\"\"" && @@records[x][0].inspect != "\"\"" 
 	formatted_records += "<tr>"
 	  for column in @@records[x]
 	    formatted_records += "<td>"+ column + "</td>"
@@ -104,6 +113,12 @@ module ApplicationHelper
   end # end while
   formatted_records += "</table>"
   formatted_final = formatted_headers + formatted_records
+  
+  if formatted_records == "</table>"
+    return "No records found that match your search."
+  else
+    return formatted_final
+  end
 end #end method
 
 end
