45390ac8ed789f1fc6c9aa3f270e6f89f07df767
derekroyse <derekroyse@gmail.com>
2014-03-23 21:31:30 -0400

    more design work, implemented (broken) exclusive search)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -113,7 +113,7 @@ module ApplicationHelper
     return formatted_final
   end # end scrape_site
   
-  def search(crn, subj, crs, sec, title, ch, max, enr, avail, wl, days, stime, etime, bldgroom, wk, instructor, ef, starts)
+  def search(crn, subj, crs, sec, title, ch, max, enr, avail, wl, days, stime, etime, bldgroom, wk, instructor, ef, starts, searchType)
 
      x = 0
      formatted_headers = "<table><tr class='table_header'>"
@@ -123,33 +123,68 @@ module ApplicationHelper
     formatted_headers += "</tr>"
     formatted_records = ""
     
-     while x < @@numRecords
-      if  @@records[x][0] == crn && crn.inspect != "\"\"" && @@records[x][0].inspect != "\"\"" || 
-	  @@records[x][1].downcase.index(subj.downcase) != nil && subj.inspect != "\"\"" && @@records[x][0].inspect != "\"\""  ||
-	  @@records[x][2].downcase.index(crs.downcase) != nil && crs.inspect != "\"\"" && @@records[x][0].inspect != "\"\""  || 
-	  @@records[x][3].downcase.index(sec.downcase) != nil && sec.inspect != "\"\"" && @@records[x][0].inspect != "\"\""  ||
-	  @@records[x][4].downcase.index(title.downcase) != nil  && title.inspect != "\"\"" && @@records[x][0].inspect != "\"\""  || 
-	  @@records[x][5] == ch && ch.inspect != "\"\"" && @@records[x][0].inspect != "\"\""  ||
-	  @@records[x][6] == max && max.inspect != "\"\"" && @@records[x][0].inspect != "\"\""  || 
-	  @@records[x][7] == enr && enr.inspect != "\"\"" && @@records[x][0].inspect != "\"\""  || 
-	  @@records[x][8] == avail && avail.inspect != "\"\"" && @@records[x][0].inspect != "\"\""  ||
-	  @@records[x][9] == wl && wl.inspect != "\"\"" && @@records[x][0].inspect != "\"\""  ||
-	  @@records[x][10].downcase.index(days.downcase) != nil && @@records[x][0].inspect != "\"\""   && days.inspect != "\"\"" || 
-	  @@records[x][11].downcase.index(stime.downcase) != nil && @@records[x][0].inspect != "\"\""  && stime.inspect != "\"\"" ||
-	  @@records[x][12].downcase.index(etime.downcase) != nil && etime.inspect != "\"\"" && @@records[x][0].inspect != "\"\""  || 
-	  @@records[x][13].downcase.index(bldgroom.downcase) != nil && bldgroom.inspect != "\"\"" && @@records[x][0].inspect != "\"\""  ||
-	  @@records[x][14].downcase.index(wk.downcase) != nil 	&& wk.inspect != "\"\"" && @@records[x][0].inspect != "\"\""  || 
-	  @@records[x][15].downcase.index(instructor.downcase) != nil && instructor.inspect != "\"\"" && @@records[x][0].inspect != "\"\""  ||
-	  @@records[x][16].downcase.index(ef.downcase) != nil 	 && ef.inspect != "\"\"" && @@records[x][0].inspect != "\"\""  || 
-	  @@records[x][17].downcase.index(starts.downcase) != nil && starts.inspect != "\"\"" && @@records[x][0].inspect != "\"\"" 
-	formatted_records += "<tr>"
-	  for column in @@records[x]
-	    formatted_records += "<td>"+ column + "</td>"
-	  end #end for
-	formatted_records += "</tr>"
-      end # end if
-      x+=1
-  end # end while
+    if searchType == "0"
+      while x < @@numRecords
+	if  @@records[x][0] == crn && crn.inspect != "\"\"" && @@records[x][0].inspect != "\"\"" || 
+	    @@records[x][1].downcase.index(subj.downcase) != nil && subj.inspect != "\"\"" && @@records[x][1].inspect != "\"\""  ||
+	    @@records[x][2].downcase.index(crs.downcase) != nil && crs.inspect != "\"\"" && @@records[x][2].inspect != "\"\""  || 
+	    @@records[x][3].downcase.index(sec.downcase) != nil && sec.inspect != "\"\"" && @@records[x][3].inspect != "\"\""  ||
+	    @@records[x][4].downcase.index(title.downcase) != nil  && title.inspect != "\"\"" && @@records[x][4].inspect != "\"\""  || 
+	    @@records[x][5] == ch && ch.inspect != "\"\"" && @@records[x][5].inspect != "\"\""  ||
+	    @@records[x][6] == max && max.inspect != "\"\"" && @@records[x][6].inspect != "\"\""  || 
+	    @@records[x][7] == enr && enr.inspect != "\"\"" && @@records[x][7].inspect != "\"\""  || 
+	    @@records[x][8] == avail && avail.inspect != "\"\"" && @@records[x][8].inspect != "\"\""  ||
+	    @@records[x][9] == wl && wl.inspect != "\"\"" && @@records[x][9].inspect != "\"\""  ||
+	    @@records[x][10].downcase.index(days.downcase) != nil && @@records[x][10].inspect != "\"\""   && days.inspect != "\"\"" || 
+	    @@records[x][11].downcase.index(stime.downcase) != nil && @@records[x][11].inspect != "\"\""  && stime.inspect != "\"\"" ||
+	    @@records[x][12].downcase.index(etime.downcase) != nil && etime.inspect != "\"\"" && @@records[x][12].inspect != "\"\""  || 
+	    @@records[x][13].downcase.index(bldgroom.downcase) != nil && bldgroom.inspect != "\"\"" && @@records[x][13].inspect != "\"\""  ||
+	    @@records[x][14].downcase.index(wk.downcase) != nil 	&& wk.inspect != "\"\"" && @@records[x][14].inspect != "\"\""  || 
+	    @@records[x][15].downcase.index(instructor.downcase) != nil && instructor.inspect != "\"\"" && @@records[x][15].inspect != "\"\""  ||
+	    @@records[x][16].downcase.index(ef.downcase) != nil 	 && ef.inspect != "\"\"" && @@records[x][16].inspect != "\"\""  || 
+	    @@records[x][17].downcase.index(starts.downcase) != nil && starts.inspect != "\"\"" && @@records[x][17].inspect != "\"\"" 
+	  formatted_records += "<tr>"
+	    for column in @@records[x]
+	      formatted_records += "<td>"+ column + "</td>"
+	    end #end for
+	  formatted_records += "</tr>"
+	end # end if
+	x+=1
+      end # end while
+    elsif searchType == "1"
+      while x < @@numRecords
+	# Test each record to see if the appropriate column
+	# matches the search request (as long as the request is not nil)
+	if  @@records[x][0] == crn || crn.inspect == "\"\"" &&
+	    @@records[x][1].downcase.index(subj.downcase) != nil || subj.inspect == "\"\"" &&
+	    @@records[x][2].downcase.index(crs.downcase) != nil || crs.inspect == "\"\"" && 
+	    @@records[x][3].downcase.index(sec.downcase) != nil || sec.inspect == "\"\"" &&
+	    @@records[x][4].downcase.index(title.downcase) != nil || title.inspect == "\"\"" && 
+	    @@records[x][5] == ch || ch.inspect == "\"\"" &&
+	    @@records[x][6] == max || max.inspect == "\"\"" &&
+	    @@records[x][7] == enr || enr.inspect == "\"\"" &&
+	    @@records[x][8] == avail || avail.inspect == "\"\"" &&
+	    @@records[x][9] == wl || wl.inspect == "\"\"" &&
+	    @@records[x][10].downcase.index(days.downcase) != nil || days.inspect == "\"\"" &&
+	    @@records[x][11].downcase.index(stime.downcase) != nil || stime.inspect == "\"\"" &&
+	    @@records[x][12].downcase.index(etime.downcase) != nil || etime.inspect == "\"\"" &&
+	    @@records[x][13].downcase.index(bldgroom.downcase) != nil || bldgroom.inspect == "\"\"" &&
+	    @@records[x][14].downcase.index(wk.downcase) != nil || wk.inspect == "\"\"" &&
+	    @@records[x][15].downcase.index(instructor.downcase) != nil || instructor.inspect == "\"\"" &&
+	    @@records[x][16].downcase.index(ef.downcase) != nil || ef.inspect == "\"\"" &&
+	    @@records[x][17].downcase.index(starts.downcase) != nil || starts.inspect == "\"\""
+	  formatted_records += "<tr>"
+	    for column in @@records[x]
+	      formatted_records += "<td>"+ column + "</td>"
+	    end #end for
+	  formatted_records += "</tr>"
+	end # end if
+	x+=1
+      end # end while
+    else
+      #nothing
+    end
+      
   formatted_records += "</table>"
   formatted_final = formatted_headers + formatted_records
   
