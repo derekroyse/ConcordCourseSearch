ddf0965ae830f22549830645a540ea019b72aee6
derekroyse <derekroyse@gmail.com>
2014-03-25 18:46:55 -0400

    help and about pages fleshed out. added modal for site codes. almost done...

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,57 @@ module ApplicationHelper
   @@numRecords = 0
   @@nilReturn = "\"\""
   @@recordsPerTable = 40
+  @@semesterArray = [
+      ['Select a Semester', 201501],	  ['Fall Semester 2014', 201501],
+      ['Second Summer Term 2014', 201405 ], ['First Summer Term 2014', 201404],  
+      ['Summer Intersession 2014', 201403], ['Spring Semester 2014', 201402],  
+      ['Fall Semester 2013', 201401], 	  ['Graduate Summer Term 2013', 201306],  
+      ['Second Summer Term 2013', 201305],  ['First Summer Term 2013', 201304], 
+      ['Summer Intersession 2013', 201303], ['Spring Semester 2013', 201302],  
+      ['Fall Semester 2012', 201301],	  ['Full Summer Term 2012', 201207],
+      ['Graduate Summer Term 2012', 201206],['Second Summer Term 2012', 201205], 
+      ['First Summer Term 2012', 201204],   ['Summer Intersession 2012', 201203],
+      ['Spring Semester 2012', 201202],     ['Fall Semester 2011', 201201],  
+      ['Full Summer Term 2011', 201107],	  ['Graduate Summer Term 2011', 201106],  
+      ['Second Summer Term 2011', 201105],  ['First Summer Term 2011', 201104],  
+      ['Summer Intersession 2011', 201103], ['Spring Semester 2011', 201102],  
+      ['Fall Semester 2010', 201101],  	  ['Full Summer Term 2010', 201007], 
+      ['Graduate Summer Term 2010', 201006],['Second Summer Term 2010', 201005],  
+      ['First Summer Term 2010', 201004],   ['Summer Intersession 2010', 201003],  
+      ['Spring Semester 2010', 201002],     ['Fall Semester 2009', 201001],  
+      ['Full Summer Term 2009', 200907],	  ['Graduate Summer Term 2009', 200906],  
+      ['Second Summer Term 2009', 200905],  ['First Summer Term 2009', 200904],  
+      ['Summer Intersession 2009', 200903], ['Spring Semester 2009', 200902], 
+      ['Fall Semester 2008', 200901],	  ['Beckley Summer Term 2008', 200807],  
+      ['Graduate Summer Term 2008', 200806],['Second Summer Term 2008', 200805],  
+      ['First Summer Term 2008', 200804],   ['Summer Intersession 2008', 200803],  
+      ['Spring Semester 2008', 200802],	  ['Fall Semester 2007', 200801], 
+      ['Graduate Summer Term 2007', 200706],['Second Summer Term 2007', 200705],  
+      ['First Summer Term 2007', 200704],   ['Summer Intersession 2007', 200703],  
+      ['Spring Semester 2007', 200702],     ['Fall Semester 2006', 200701],  
+      ['Graduate Summer Term 2006', 200606],['Second Summer Term 2006', 200605],  
+      ['First Summer Term 2006', 200604],   ['Summer Intersession 2006', 200603],  
+      ['Spring Semester 2006', 200602],  	  ['Fall Semester 2005', 200601],  
+      ['Graduate Summer Term 2005', 200506],['Second Summer Term 2005', 200505],  
+      ['First Summer Term 2005', 200504],	  ['Summer Intersession 2005', 200503],  
+      ['Spring Semester 2005', 200502], 	  ['Fall Semester 2004', 200501],  
+      ['Graduate Summer Term 2004', 200406],['Second Summer Term 2004', 200405],  
+      ['First Summer Term 2004', 200404],   ['Summer Intersession 2004', 200403],  
+      ['Spring Semester 2004', 200402],     ['Fall Semester 2003', 200401],  
+      ['Second Summer Term 2003', 200305],  ['First Summer Term 2003', 200304],  
+      ['Summer Intersession 2003', 200303], ['Spring Semester 2003', 200302],  
+      ['Fall Semester 2002', 200301],	  ['Second Summer Term 2002', 200205],  
+      ['First Summer Term 2002', 200204],   ['Summer Intersession 2002', 200203],  
+      ['Spring Semester 2002', 200202],	  ['Fall Semester 2001', 200201],  
+      ['Second Summer Term 2001', 200105],  ['First Summer Term 2001', 200104],  
+      ['Summer Intersession 2001', 200103], ['Spring Semester 2001', 200102],  
+      ['Fall Semester 2000', 200101],  	  ['Second Summer Term 2000', 200005],  
+      ['First Summer Term 2000', 200004],	  ['Summer Intersession 2000', 200003],  
+      ['Spring Semester 2000', 200002],     ['Fall Semester 1999', 200001]]
+  
+  def getSemesterArray()
+    return @@semesterArray
+  end
   
   # Return a formatted semester string
   def getSemester(choice)
