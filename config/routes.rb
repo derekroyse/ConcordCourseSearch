b268bbaac2f67d80834b835e36bd121c24fb7557
derekroyse <derekroyse@gmail.com>
2017-01-01 18:20:43 -0500

    heroku compatibiilty

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 SampleApp::Application.routes.draw do
   get "scraper/new"
   root 'static_pages#home'
-  match '/startup', to: 'scraper#startup', via: 'get'
-  match '/startup', to: 'scraper#startup', via: 'post'
-  match '/search', to: 'scraper#search', via: 'get'
-  match '/search', to: 'scraper#search', via: 'post'
+  match '/startup', to: 'static_pages#startup', via: 'get'
+  match '/startup', to: 'static_pages#startup', via: 'post'
+  match '/search', to: 'static_pages#search', via: 'get'
+  match '/search', to: 'static_pages#search', via: 'post'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
