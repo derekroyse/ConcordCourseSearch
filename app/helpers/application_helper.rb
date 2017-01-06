1552a16fd96f5ce8650fc0eeae60f76e75de0d6f
derekroyse <derekroyse@gmail.com>
2017-01-06 07:34:46 -0500

    testing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -184,7 +184,7 @@ module ApplicationHelper
 			    # @@conversion[i+16] + "','" + @@conversion[i+17] + "')"
 		queryString3 = "INSERT IGNORE INTO SEMESTER201501(CRN, SUBJ, CRS, SEC, TITLE, CH, MAX, ENR, 
 							AVAIL, WL, DAYS, STIME, ETIME, ROOM, WK, INSTRUCTOR, EF, STARTSON)
-							VALUES(1,TEST,1,1,TEST,1,1,1,1,1,TEST,1,1,TEST,1,TEST,TEST,TEST)"	
+							VALUES(1,'TEST',1,1,'TEST',1,1,1,1,1,'TEST',1,1,'TEST',1,'TEST','TEST','TEST')"	
 	    connection.query(queryString3)
 	  i+=17
       end # end if/else
