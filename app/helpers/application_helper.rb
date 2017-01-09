096533a74cbe6d630d53717d68a0a63fe65f9098
derekroyse <derekroyse@gmail.com>
2017-01-09 09:42:16 -0500

    heroku

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,7 +15,7 @@ module ApplicationHelper
    #                 ['First Summer Term 2014', 201404], ['Summer Intersession 2014', 201403], 
    #                 ['Spring Semester 2014', 201402]]
 					
-	@@semesterArray = [['Select a Semester', 201701]]				
+	@@semesterArray = [['Fall Semester 2016', 201701]]				
 					
 	# @@semesterArray = [['Select a Semester', 201701], 	  ['Fall Semester 2016', 201701],
 						# ['Second Summer Term 2016', 201605],		['First Summer Term 2016', 201604],
